20cfc5b08837f2084c5e949419bdedc3ff47ad87
Freddy Drennan <drennanfreddy@gmail.com>
2020-09-21 21:22:47 -0600

    change type

--- R/with_console_redirect.R
+++ R/with_console_redirect.R
@@ -6,7 +6,6 @@
 #' @export withConsoleRedirect
 withConsoleRedirect <- function(containerId, expr) {
   message('Running Code')
-  print('Running Doce')
   # Change type="output" to type="message" to catch stderr
   # (messages, warnings, and errors) instead of stdout.
   txt <- capture.output(results <- expr, type = "output")
